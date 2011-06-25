6be06fea3e13ff9608baed3c3bfee1ee755075cf
makism <makhsm@gmail.com>
2011-06-25 22:10:57 +0300

    * Added method ToString. * Implemented destructor to release memory. * Implemented ctors.

--- headers/Particle.hpp
+++ headers/Particle.hpp
@@ -7,14 +7,15 @@ namespace psocxx {
 class Particle {
 public:
     Particle(void);
+    Particle(const Vector& position);
     Particle(const Vector& position, const Vector& velocity);
     Particle(const Particle& n);
     virtual ~Particle(void);
 
     long unsigned int Id(void) const;
-
-    virtual const std::string ToString(void) const;
     
+    const std::string ToString(void) const;
+
 protected:
     /*! */
     Vector *mPosition;
--- src/Particle.cpp
+++ src/Particle.cpp
@@ -2,5 +2,53 @@
 
 
 namespace psocxx {
+    
+Particle::Particle(void)
+{
+
+}
+
+
+Particle::Particle(const Vector& position)
+    : mVelocity(0)
+{
+    mPosition = new Vector(position);
+    mBestPosition = new Vector(position);
+}
+
+Particle::Particle(const Vector& position, const Vector& velocity)
+{
+    mPosition = new Vector(position);
+    mVelocity = new Vector(velocity);
+    mBestPosition = new Vector(position);
+}
+
+Particle::Particle(const Particle& n)
+{
+    
+}
+
+Particle::~Particle(void)
+{
+    delete mPosition;
+    delete mVelocity;
+    delete mBestPosition;
+}
+
+const std::string Particle::ToString(void) const
+{
+    std::ostringstream oss;
+    
+    oss << "Particle: Position(" << mPosition->ToString() << ")";
+    
+    if (mVelocity!=0)
+        oss << ", Velocity(" << mVelocity->ToString() << ")";
+    
+    if (mBestPosition!=0)
+        oss << ", Best Position(" << mBestPosition->ToString() << ")";
+    
+    return oss.str();
+}
+
 
 }
