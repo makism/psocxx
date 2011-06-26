f07fb32c6f1a134282e9a984219f760a3e5e1cab
makism <makhsm@gmail.com>
2011-06-26 12:20:09 +0300

    * Modified Random class to support random real numbers for Position (U(lo, hi)) and Velocity (U(-|hi-lo|, |hi-lo|)).

--- helpers/Random.cpp
+++ helpers/Random.cpp
@@ -3,20 +3,36 @@
 
 namespace psocxx {
 namespace Helpers {
+
+Random* Random::mInstance = 0;
+
+Random::Random(float lo, float hi)
+{
+    baseGen = new boost::minstd_rand(static_cast<unsigned int>(Seed()));
+
+    uniDblUnit = new boost::uniform_real<>(lo, hi);
+    uniDblGen = new boost::variate_generator<boost::minstd_rand&, boost::uniform_real<> >(*baseGen, *uniDblUnit);
+    
+    uniDblUnit_Vel = new boost::uniform_real<>(-(abs(hi-lo)), abs(hi-lo));
+    uniDblGen_Vel = new boost::variate_generator<boost::minstd_rand&, boost::uniform_real<> >(*baseGen, *uniDblUnit_Vel);
+}
+
 Random::~Random(void) {
     delete uniDblGen;
+    delete uniDblGen_Vel;
     delete uniDblUnit;
+    delete uniDblUnit_Vel;
     delete baseGen;
 }
 
-double Random::Generate(void) const {
-    return (*uniDblGen)();
+float Random::Position(void) const
+{
+    return static_cast<float>((*uniDblGen)());
 }
 
-Random::Random(float lo, float hi) {
-    baseGen = new boost::minstd_rand(static_cast<unsigned int>(Seed()));
-    uniDblUnit = new boost::uniform_real<>(lo, hi);
-    uniDblGen = new boost::variate_generator<boost::minstd_rand&, boost::uniform_real<> >(*baseGen, *uniDblUnit);
+float Random::Velocity(void) const
+{
+    return static_cast<float>((*uniDblGen_Vel)());
 }
 
 #ifdef unix
@@ -36,8 +52,6 @@ uint64_t Random::Seed() const;
     return time(NULL);
 }
 #endif
-
-Random* Random::mInstance = 0;
 
 }
 }
--- helpers/Random.hpp
+++ helpers/Random.hpp
@@ -14,17 +14,21 @@
          // and http://sci.tuomastonteri.fi/programming/cplus/wrapper-to-boost-random
          // !!! needs optimization !!!
          class Random {
-         public:
-             ~Random(void);
              
-             static Random* Instance(float lo=0.0f, float hi=1.0f) {
+         public:
+             static Random* Instance(float lo=0.0f, float hi=1.0f)
+             {
                  if (mInstance==0)
                      mInstance = new Random(lo, hi);
                  
                  return mInstance;
              }
              
-             double Generate(void) const;
+         public:
+             ~Random(void);
+             
+             float Position(void) const;
+             float Velocity(void) const;
              
          private:
              Random(float lo, float hi);
@@ -34,8 +38,12 @@
              static Random* mInstance;
              
              boost::minstd_rand * baseGen;
+             
              boost::uniform_real<> * uniDblUnit;
              boost::variate_generator<boost::minstd_rand&, boost::uniform_real<> > * uniDblGen;
+             
+             boost::uniform_real<> * uniDblUnit_Vel;
+             boost::variate_generator<boost::minstd_rand&, boost::uniform_real<> > * uniDblGen_Vel;
          };
      }
  }
