3b0a08a0acad5c77cda7c909d64f37aef399041e
makism <makhsm@gmail.com>
2011-06-26 12:46:38 +0300

    * Updated to match the changes in API.

--- examples/Test.cpp
+++ examples/Test.cpp
@@ -2,14 +2,29 @@
 
 using namespace psocxx;
 
+namespace psocxx {
+    float Particle::EvaluateCallback(const Vector* v)
+    {
+        return 0.0f;
+    }
+}
+
 int main(int argc, char** arv)
 {
     Swarm *s = new Swarm(100);
-    s->ConfigureSpace(3, 0.5f, 1.0f);
+    
+    // Define a 3 dimentional space.
+    // The boundaries: 0.5f (lower) and 1.5f (upper).
+    s->ConfigureSpace(3, 0.5f, 1.5f);
+    
+    // Pass the parameters (omega, phi_p, phi_g).
     s->SetParameters(0.0f, 0.0f, 0.0f);
+    
+    // Create inital particles.
     s->Init();
     
-    delete s;
     
+    delete s;
+
     return 0;
 }
