ec88e5b1dcd80d73479fd23269a385fc33a1435f
makism <makhsm@gmail.com>
2011-06-30 16:12:10 +0300

    * Removed net, replaced with axis.

--- examples/DeJong.cpp
+++ examples/DeJong.cpp
@@ -55,30 +55,6 @@ void ReSizeGLScene(int w, int h)
     glMatrixMode(GL_MODELVIEW);
 }
 
-void DrawNet(GLfloat size, GLint LinesX, GLint LinesZ)
-{
-    glBegin(GL_LINES);
-    for (int xc = 0; xc < LinesX; xc++)
-    {
-        glVertex3f( -size / 2.0 + xc / (GLfloat)(LinesX-1)*size,
-                    0.0,
-                    size / 2.0);
-        glVertex3f( -size / 2.0 + xc / (GLfloat)(LinesX-1)*size,
-                    0.0,
-                    size / -2.0);
-    }
-    for (int zc = 0; zc < LinesX; zc++)
-    {
-        glVertex3f( size / 2.0,
-                    0.0,
-                    -size / 2.0 + zc / (GLfloat)(LinesZ-1)*size);
-        glVertex3f( size / -2.0,
-                    0.0,
-                    -size / 2.0 + zc / (GLfloat)(LinesZ-1)*size);
-    }
-    glEnd();
-}
-
 void Display()
 {
     glClear(GL_COLOR_BUFFER_BIT);
@@ -87,35 +63,15 @@ void Display()
     Camera.Render();
 
     // Draw the axis
-    GLfloat size = 2.0;
-    GLint LinesX = 30;
-    GLint LinesZ = 30;
-    
-    GLfloat halfsize = size / 2.0;
-    glColor3f(1.0,1.0,1.0);
-    glPushMatrix();
-    glTranslatef(0.0,-halfsize ,0.0);
-    DrawNet(size,LinesX,LinesZ);
-    glTranslatef(0.0,size,0.0);
-    DrawNet(size,LinesX,LinesZ);
-    glPopMatrix();
-    glColor3f(0.0,0.0,1.0);
-    glPushMatrix();
-    glTranslatef(-halfsize,0.0,0.0);    
-    glRotatef(90.0,0.0,0.0,halfsize);
-    DrawNet(size,LinesX,LinesZ);
-    glTranslatef(0.0,-size,0.0);
-    DrawNet(size,LinesX,LinesZ);
-    glPopMatrix();
-    glColor3f(1.0,0.0,0.0);
-    glPushMatrix();
-    glTranslatef(0.0,0.0,-halfsize);    
-    glRotatef(90.0,halfsize,0.0,0.0);
-    DrawNet(size,LinesX,LinesZ);
-    glTranslatef(0.0,size,0.0);
-    DrawNet(size,LinesX,LinesZ);
-    glPopMatrix();
-    
+    glBegin(GL_LINES);
+    glColor4f(1,1,1,0);
+    glVertex3f(-20, 0, 0);
+    glVertex3f(+20, 0, 0);
+    glVertex3f(0, +20, 0);
+    glVertex3f(0, -20, 0);
+    glVertex3f(0, 0, -20);
+    glVertex3f(0, 0, +20);
+    glEnd();
     
     // Draw the function.
     glPointSize(2.0);
